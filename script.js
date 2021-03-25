164afe048e1dfd9a1ff05c1397426b6a9de7f427
vigneshcseloyola <80444328+vigneshcseloyola@users.noreply.github.com>
2021-03-25 17:50:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,80 +1,41 @@
-window.onload = () => {
-console.log('hello world....');
-    addBtnFunc();
-    addcopyFunc();
-    addTextBoxFunc();
-}
-
-function addBtnFunc() {
-     document.getElementById('btn1').addEventListener('click', ()=> {
-        getValue.call(this);
-     });   
-}
-
-function addTextBoxFunc() {
-    document.getElementById('addBtn').addEventListener('click', ()=> {
-        console.log('adding the boxes');
-        var input = document.createElement('input'); 
-        input.setAttribute("type", "text");
-        input.setAttribute("value", "");
-        input.setAttribute("class", "search");
-        input.setAttribute('style', 'top:25%');
-        document.getElementById("place").appendChild(input);
-    })
-}
-
- function generateit()
-    {
-        return'<input type="text"><button onclick="removeit(this)"> - </button>';  
-    }
-    
-    function removeit(input)
-    {
-        document.getElementById("place").removeChild(input.parentNode);
-    }
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+      document.getElementById("user-div").style.display= "block ";
+      document.getElementById("login-div").style.display= "none";
+      
+      var user = firebase.auth().currentUser;
 
 
-function addTextBoxFunc() {
-    document.getElementById('addBtn').addEventListener('click', ()=> {
-        console.log('adding the boxes');
-        var input = document.createElement('input'); 
-        input.setAttribute("type", "text");
-        input.setAttribute("value", "");
-        input.setAttribute("class", "search");
-        input.setAttribute('style', 'top:25%');
-        document.getElementById("place").appendChild(input);
-    })
-}
+if (user != null) {
+    var name, email;
+  
+    document.getElementById("user-para").innerHTML="Welcome User:" +email: +password;
+      
+  } else { 
+      
+      document.getElementById("user-div").style.display= "none";
+      document.getElementById("login-div").style.display= "initial";
+  }
+});
+ 
+function login(){
+var userEmail = document.getElementById("email-field").value;
+var userpass = document.getElementById("password-field").value;
 
-function addcopyFunc() {
-    document.getElementById('btn2').addEventListener('click', () => {
-        var final = document.getElementById('txtbox1').value
-        if(final) {
-            navigator.clipboard.writeText(final).then(function() {
-                alert('Copying to clipboard was successful!');
-            }, function(err) {
-                alert('Could not Copy URL: ');
-            });
-        } else {
-            alert('Could not Copy URL: ');
-        }
-    });
-}
-
-function getValue() {
-var textValue = document.getElementById('txtbox');
-    if(textValue && textValue.value) {
-        console.log('value', textValue.value);
-        appendToTxtBox.call(this, textValue.value);        
-    } else {
-        console.log('It has empty value');
-        appendToTxtBox.call(this, '');
-    }
-}
-
-function appendToTxtBox(value) {
-    if(true) {
-        document.getElementById('txtbox1').value = value;
+    firebase.auth().signInWithEmailAndPassword(useremail, userpass)
+  .then((userCredential) => {
+    var user = userCredential.user;
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
         
-    }
+        window.alert("Error:" + errorMessage);
+  });
+
 }
+      
+function logout(){
+    firebase.auth().signOut()
+}
+      
